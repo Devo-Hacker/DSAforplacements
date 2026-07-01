43b6e7c11f92a858ad7d39fe28468931d16653cf
Devo-hacker <niladreebiharinayak@gmail.com>
2026-07-01 23:03:38 +0530

    updated commit

---
+++ LinkedList/linkedListBasics.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <list>
+#include <vector>
+using namespace std; 
+struct Node {
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+};
+
+int main(){
+    vector<int> arr = {1, 2, 4, 5 ,6, 7, 3};
+    Node* y = new Node(arr[0], nullptr);
+    cout << y;
+}
