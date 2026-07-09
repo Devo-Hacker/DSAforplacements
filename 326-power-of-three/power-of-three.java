da9b21f0a097744d166ab5054f8e16ee4daddf3b
Niladree Bihari Nayak <niladreebiharinayak@gmail.com>
2026-07-09 09:03:31 +0530

    Time: 8 ms (91.83%) | Memory: 46.1 MB (25.09%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n<0 || n==0) return false;
+        // if(n<0 || n==0) return false;
         return (n>0 && Math.pow(3,19) % n == 0);
     }
 }
