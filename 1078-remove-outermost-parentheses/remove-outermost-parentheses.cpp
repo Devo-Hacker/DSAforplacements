a20b7a7fe13289fc7e5f1d76b8c6644f02158007
Niladree Bihari Nayak <niladreebiharinayak@gmail.com>
2026-07-15 10:37:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (97.80%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int counter = 0;
+        string ans = "";
+        for(int i = 0; i<s.size(); i++){
+            if(s[i] == ')'){
+                counter--;
+            }
+            if(counter != 0){
+                ans += s[i];
+            }
+            if(s[i] == '('){
+                counter++;
+            }
+        }
+        return ans;
+    }
+};
