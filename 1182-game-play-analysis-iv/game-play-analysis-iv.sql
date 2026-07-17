6278339d944d27eeb2d6a9ca004c8c97924c9a21
Niladree Bihari Nayak <niladreebiharinayak@gmail.com>
2026-07-17 14:01:57 +0530

    Time: 550 ms (77.59%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select ROUND(COUNT(DISTINCT player_id) / (select count(distinct player_id) from activity), 2) as fraction from Activity where (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) IN (SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id)
