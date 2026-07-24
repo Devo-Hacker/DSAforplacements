f905be8082d924ff95b91bc1c553c9b2efb424e9
Niladree Bihari Nayak <niladreebiharinayak@gmail.com>
2026-07-24 21:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (56.84%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int rich = 0;
+        for(int i = 0; i<accounts.size(); i++){
+            int total = 0;
+            for(int j = 0; j<accounts[i].size(); j++){
+                total += accounts[i][j];
+                if(rich<total){
+                    rich = total;
+                }
+            }
+        }
+        return rich;
+    }
+};
